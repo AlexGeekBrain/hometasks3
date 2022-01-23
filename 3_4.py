0751a72e6e5ead9c34abe32f5c20902b97256323
ALEXEY SOBOLEV <96187663+AlexGeekBrain@users.noreply.github.com>
2022-01-23 15:28:55 +0300

    Delete 3_4.py

--- 3_4.py
+++
@@ -1,9 +0,0 @@
-def thesaurus_avd(*args):
-    fn_ln_sort = {}
-    for fn_ln in args:
-        fn_ln_sort.setdefault(fn_ln.split()[1][0], {}).setdefault(fn_ln.split()[0][0], []).append(fn_ln)
-    return fn_ln_sort
-
-
-print(thesaurus_avd('Иван Сергеев', 'Инна Серова', 'Иван Алексеев', 'Борис Серов', 'Алексей Иванов',
-                    'Павел Борисов', 'Евгений Родионов', 'Жанна Родионова', 'Оксана Борисова'))
